14f1a9acabfdb4b99e2e4a100e5c397f85fd683f
Jamal Shamsi <jamal.shamsi1988@gmail.com>
2023-09-22 11:08:03 +0330

    loge out api created & user can loge out by clear the token

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,18 @@ import  Link  from 'next/link';
 import styles from '../styles/Home.module.css'
 
  function Home() {
+
+  const logeOutHandler = async ()=>{
+    const res = await fetch("/api/auth/signout");
+    const data = await res.json();
+    console.log(data)
+  }
   return (
     <div className={styles.container}>
        <button><Link href="/dashbord">Dashbord</Link></button>
       <button><Link href="/signup">Sign Up</Link></button>
       <button><Link href="/signin">Sign In</Link></button>
+      <button onClick={logeOutHandler}>Loge Out</button>
     </div>
   )
 }

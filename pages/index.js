3af0956894fd0135d0ed3ad6b40f62fe10f75190
Jamal Shamsi <jamal.shamsi1988@gmail.com>
2023-09-22 11:26:01 +0330

    loge out button handle

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,41 @@
 import  Link  from 'next/link';
 
 import styles from '../styles/Home.module.css'
+import { useEffect, useState } from 'react';
 
  function Home() {
+  const [isLogedIn , setIsLogedIn]=useState(false);
+
+  useEffect(()=>{
+    fetch("/api/user").then(res => res.json()).then(data =>{
+      if(res.status === "success") setIsLogedIn(true);
+    })
+  },[])
 
   const logeOutHandler = async ()=>{
     const res = await fetch("/api/auth/signout");
     const data = await res.json();
-    console.log(data)
+    if(data.status === "success") isLogedIn(false);
   }
   return (
     <div className={styles.container}>
-       <button><Link href="/dashbord">Dashbord</Link></button>
-      <button><Link href="/signup">Sign Up</Link></button>
-      <button><Link href="/signin">Sign In</Link></button>
-      <button onClick={logeOutHandler}>Loge Out</button>
+        {
+          isLogedIn ? (
+          <>
+              <button><Link href="/dashbord">Dashbord</Link></button>
+              <button onClick={logeOutHandler}>Loge Out</button>
+          </> ) : null
+        }
+
+        {
+          !isLogedIn ? (
+          <>
+          <button><Link href="/signup">Sign Up</Link></button>
+           <button><Link href="/signin">Sign In</Link></button>
+          </>) : null
+        }
+   
+      
     </div>
   )
 }
